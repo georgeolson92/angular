4e53dad7656b086d670a3da33f5834ce5dd47596
George <georgeolson92@gmail.com>
2016-08-09 12:14:14 -0700

    Add inline event

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,26 @@
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>To-Do List</h1>
-    <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
-  </div>
+    <div class="container">
+      <h1>To-Do List</h1>
+      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
+    </div>
   `
 })
-
 export class AppComponent {
   public tasks: Task[];
   constructor(){
     this.tasks = [
       new Task("Create To-Do List app.", 0),
       new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lorde of the Rings", 2),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
     ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
   }
 }
 
