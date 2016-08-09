338e4bdabf76dcfebaa1282ac21305f053e5dd38
George <georgeolson92@gmail.com>
2016-08-09 13:53:12 -0700

    Add button, input to add new task to list

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 @Component({
     selector: 'task-list',
     inputs: ['taskList'],
     outputs: ['onTaskSelect'],
-    directives: [TaskComponent],
-    templateUrl: 'app/task-list.component.html' 
+    directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
+    templateUrl: 'app/task-list.component.html'
 })
 export class TaskListComponent {
   public taskList: Task[];
@@ -20,5 +22,10 @@ export class TaskListComponent {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
   }
 }
