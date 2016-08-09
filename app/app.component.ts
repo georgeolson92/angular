5b8f21ae3a26a65dabd26370c1be10cc925462c6
George <georgeolson92@gmail.com>
2016-08-09 12:05:38 -0700

    Initial commit - fully functional Angular app

---
+++ app/app.component.ts
@@ -0,0 +1,30 @@
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>To-Do List</h1>
+    <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+  </div>
+  `
+})
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lorde of the Rings", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){
+
+  }
+}
+
+import { Component } from 'angular2/core';
