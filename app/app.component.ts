dcd54c765641d639df33c1df36b407ad2dd78186
George <georgeolson92@gmail.com>
2016-08-09 13:02:00 -0700

    Add selector

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,54 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+    `
+})
+
+export class TaskComponent {
+  public task: Task;
+}
+
+@Component({
+    selector: 'task-list',
+    inputs: ['taskList'],
+    outputs: ['onTaskSelect'],
+    directives: [TaskComponent],
+  template: `
+    <div class="container">
+      <task-display *ngFor="#currentTask of taskList"
+      (click)="taskClicked(currentTask)"
+      [class.selected]="currentTask === selectedTask"
+      [task]="currentTask">
+        {{ currentTask.description }}
+      </task-display>
+    </div>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
-        {{ task.description }}
-      </h3>
+      <task-list [taskList]="tasks" (conTaskSelect)="taskWasSelected($event)"></task-list>
     </div>
   `
 })
@@ -30,5 +73,3 @@ export class Task {
 
   }
 }
-
-import { Component } from 'angular2/core';
