8f05a72610bd2d1d98176a2f4ff83085b4451ae7
George <georgeolson92@gmail.com>
2016-08-09 13:18:15 -0700

    Separate modules

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,46 +1,7 @@
 import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
-@Component({
-    selector: 'task-display',
-    inputs: ['task'],
-  template: `
-    <h3>{{ task.description }}</h3>
-    `
-})
-
-export class TaskComponent {
-  public task: Task;
-}
-
-@Component({
-    selector: 'task-list',
-    inputs: ['taskList'],
-    outputs: ['onTaskSelect'],
-    directives: [TaskComponent],
-  template: `
-    <div class="container">
-      <task-display *ngFor="#currentTask of taskList"
-      (click)="taskClicked(currentTask)"
-      [class.selected]="currentTask === selectedTask"
-      [task]="currentTask">
-        {{ currentTask.description }}
-      </task-display>
-    </div>
-  `
-})
-export class TaskListComponent {
-  public taskList: Task[];
-  public onTaskSelect: EventEmitter<Task>;
-  public selectedTask: Task;
-  constructor() {
-    this.onTaskSelect = new EventEmitter();
-  }
-  taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
-    this.selectedTask = clickedTask;
-    this.onTaskSelect.emit(clickedTask);
-  }
-}
 
 @Component({
   selector: 'my-app',
@@ -48,8 +9,11 @@ export class TaskListComponent {
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <task-list [taskList]="tasks" (conTaskSelect)="taskWasSelected($event)"></task-list>
-    </div>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
   `
 })
 export class AppComponent {
@@ -63,13 +27,6 @@ export class AppComponent {
     ];
   }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number){
-
+    console.log('parent', clickedTask);
   }
 }
